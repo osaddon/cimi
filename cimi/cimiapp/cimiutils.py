29a93ba254b8b7036f98265263f5db03230cc779
Tong Li <litong01@us.ibm.com>
2012-09-13 22:48:11 -0400

    fix up all the license headers in all the files.

--- cimi/cimiapp/cimiutils.py
+++ cimi/cimiapp/cimiutils.py
@@ -1,18 +1,17 @@
-# Copyright (c) 2011 IBM
+# Copyright (c) 2012 IBM
 #
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
 #
-#    http://www.apache.org/licenses/LICENSE-2.0
+#         http://www.apache.org/licenses/LICENSE-2.0
 #
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
-# implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+#
 # This is the module which defines some useful functions for cimi
 # implementation. This module should not reference any methods defined
 # in cimi implementation modules. This is to avoid any circular reference
